85db3c519b438535fc3472b49a64c8887406f8d8
Huan Luo <dukeluo@outlook.com>
2025-01-14 20:58:11 +0800

    refactor: migrate to page router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,13 @@
-'use client'
-
 import { useState, useEffect } from 'react'
-import StockRecommendation from './components/StockRecommendation'
-import { fetchStockData } from './services/stockService'
+import StockRecommendation from '../components/StockRecommendation'
+import { fetchStockData } from '../services/stockService'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 
-export default function NewTabPage() {
+export default function HomePage() {
   const [stockData, setStockData] = useState<Array<{ code: string; name: string }>>([])
   const [currentDay, setCurrentDay] = useState(0)
 
-  console.log('im here 111111......')
   useEffect(() => {
-    console.log('im here......')
     fetchStockData().then(data => setStockData(data))
   }, [])
 
@@ -24,8 +20,8 @@ export default function NewTabPage() {
   }
 
   return (
-    <div 
-      className="min-h-screen w-full flex items-center justify-center relative bg-white"
+    <div
+      className="h-full w-full flex items-center justify-center relative bg-white"
       style={{
         backgroundImage: 'url("https://hebbkx1anhila5yf.public.blob.vercel-storage.com/bg3-EuVOlkhWh4gxSnfOnBzim1C3eNJTOz.jpeg")',
         backgroundPosition: 'center',
@@ -38,16 +34,16 @@ export default function NewTabPage() {
           <StockRecommendation stock={stockData[currentDay]} />
         )}
       </div>
-      <button 
-        onClick={handlePrevDay} 
+      <button
+        onClick={handlePrevDay}
         disabled={currentDay === 0}
         className="absolute left-0 top-1/2 transform -translate-y-1/2 px-2 py-20 bg-black/10 hover:bg-black/20 text-black rounded-r transition-opacity duration-300 opacity-0 hover:opacity-100"
         aria-label="Previous Day"
       >
         <ChevronLeft className="w-8 h-8" />
       </button>
-      <button 
-        onClick={handleNextDay} 
+      <button
+        onClick={handleNextDay}
         disabled={currentDay === 6}
         className="absolute right-0 top-1/2 transform -translate-y-1/2 px-2 py-20 bg-black/10 hover:bg-black/20 text-black rounded-l transition-opacity duration-300 opacity-0 hover:opacity-100"
         aria-label="Next Day"
@@ -57,4 +53,3 @@ export default function NewTabPage() {
     </div>
   )
 }
-
